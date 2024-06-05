7959abab97c2d1fefe24c41bd7e5b8f2c4b4f272
aspes <asopes@gmail.com>
2024-06-05 11:00:21 +0900

    Resilient server upgrade

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -15,3 +15,10 @@ public class PayrollApplication {
 // curl -v localhost:8080/employees
 // curl -X POST localhost:8080/employees -H 'Content-type:application/json' -d '{"name": "Samwise Gamgee", "role": "gardener"}'
 // curl -X DELETE localhost:8080/employees/3
+
+
+/*
+Restful app is resilient.
+A key factor of resilience is that when you make upgrades to your services, your clients do not suffer downtime.
+Keep db columns for backward compatibility.
+ */
