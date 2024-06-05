6deebb5a10f5315788a0db1b0d42c63b99cf4e03
aspes <asopes@gmail.com>
2024-06-05 17:26:05 +0900

    Resilient server upgrade without affecting clients

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -5,17 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayrollApplication {
-
     public static void main(String... args) {
         SpringApplication.run(PayrollApplication.class, args);
     }
 }
 
 // ./mvnw clean spring-boot:run
-// curl -v localhost:8080/employees
-// curl -X POST localhost:8080/employees -H 'Content-type:application/json' -d '{"name": "Samwise Gamgee", "role": "gardener"}'
-// curl -X DELETE localhost:8080/employees/3
-
+// curl -v localhost:8080/employees // read
+// curl -X POST localhost:8080/employees -H 'Content-type:application/json' -d '{"name": "Samwise Gamgee", "role": "gardener"}' // create
+// curl -v -X POST localhost:8080/employees -H 'Content-Type:application/json' -d '{"name": "Samwise Gamgee", "role": "gardener"}' | json_pp
+// curl -X PUT localhost:8080/employees/2 -H 'Content-type:application/json' -d '{"name": "Samwise Gamgee", "role": "gardener"}' // update
+// curl -X DELETE localhost:8080/employees/3 // delete
 
 /*
 Restful app is resilient.
